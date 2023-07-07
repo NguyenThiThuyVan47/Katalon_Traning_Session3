f980d34dea48db511f0b4560c299706e3d57fa56
Van Nguyen Thi Thuy <ntthuyvan1@tma.com.vn>
2023-07-08 00:26:47 +0700

    adding dynamic TS 1

---
+++ Test Suites/New Dynamic Test Suite 1.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<FilteringTestSuiteEntity>
+   <description></description>
+   <name>New Dynamic Test Suite 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b06e10a7-c154-4158-b7da-67b5c9db4cf5</testSuiteGuid>
+   <filteringText>tag=(Order_TC001) </filteringText>
+</FilteringTestSuiteEntity>
